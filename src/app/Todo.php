bf186224ceee19b69f242db3563e4e6a7cb64b0b
y-sco <yuuki091005@gmail.com>
2023-11-15 16:45:22 +0900

    solve: fillメソッドを使用した方がいい理由を記載した。

--- src/app/Todo.php
+++ src/app/Todo.php
@@ -11,4 +11,6 @@ class Todo extends Model
     protected $fillable = [
         'content'
     ];
+    // fillableプロパティを使用することで、モデルクラスでfillメソッドを使用して、データを保存することができる。
+    // 今回は'content' カラムだけだが、複数のカラムを保存するときにfillメソッドを使用することで、記述する量を省くことができる。
 }
