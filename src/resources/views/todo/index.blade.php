3ad11f1b81ebd9e1b33b8f8afb96eecf40af429b
Yuuki Fujimori <yuuki091005@gmail.com>
2025-04-21 15:34:15 +0900

    add: dbから取得したtodoデータをviewにて一覧表示

--- src/resources/views/todo/index.blade.php
+++ src/resources/views/todo/index.blade.php
@@ -32,6 +32,13 @@
               <div class="card-header">
                 ToDo一覧
               </div>
+                <div class="list-group list-group-flush">
+                  @foreach ($todos as $todo)
+                    <div class="d-flex align-items-center p-2">
+                      <span class="col-9">{{ $todo->content }}</span>
+                    </div>
+                  @endforeach
+                </div>
               <div class="list-group list-group-flush">
               </div>
             </div>
